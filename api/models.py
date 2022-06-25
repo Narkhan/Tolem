4b38c615c16bd8ad418aec293c2e7e73ef2693a3
gmkanat <kanat_9999@inbox.ru>
2022-06-25 13:04:19 +0600

    added User & Manager models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,78 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    username = models.CharField(
+        max_length=50,
+        verbose_name='Username',
+        unique=True,
+        help_text='Username for login'
+    )
+    password = models.CharField(
+        max_length=50,
+        verbose_name='Password',
+        help_text='Password for login'
+    )
+    email = models.EmailField(
+        max_length=254,
+        verbose_name='Email',
+        unique=True,
+        help_text='Email for login'
+    )
+    first_name = models.CharField(
+        max_length=50,
+        verbose_name='First Name',
+        help_text='First Name'
+    )
+    last_name = models.CharField(
+        max_length=50,
+        verbose_name='Last Name',
+        help_text='Last Name'
+    )
+    is_active = models.BooleanField(
+        default=True,
+        verbose_name='Is Active',
+        help_text='Is Active'
+    )
+    is_admin = models.BooleanField(
+        default=False,
+        verbose_name='Is Admin',
+        help_text='Is Admin'
+    )
+    phone = models.CharField(
+        max_length=50,
+        verbose_name='Phone'
+    )
+    is_staff = models.BooleanField(
+        default=False,
+        verbose_name='Is Staff',
+        help_text='Is Staff'
+    )
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.username
+
+
+class Manager(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='User manager',
+        help_text='User manager'
+    )
+    # FIXME: need to change to foreign key
+    company = models.CharField(
+        max_length=128,
+        verbose_name='Company'
+    )
+
+    class Meta:
+        verbose_name = 'Manager'
+        verbose_name_plural = 'Managers'
+
+    def __str__(self):
+        return self.user.username
