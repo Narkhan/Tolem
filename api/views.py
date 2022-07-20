17fb001ec54c8c32c994c51a70deb68c65c63fe7
nkamzabek <narkhan2002@gmail.com>
2022-07-20 16:05:58 +0600

    MAIN-6&9: Implemented methods and added drf spectacular

--- api/views.py
+++ api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import mixins
+from rest_framework.viewsets import GenericViewSet
+
+from api.models.restaurant import Restaurant, Menu
+from api.serializers import RestaurantSerializer, MenuSerializer
+
+
+class RestaurantViewSet(mixins.ListModelMixin,
+                        GenericViewSet):
+    queryset = Restaurant.objects.all()
+    serializer_class = RestaurantSerializer
+
+
+class MenuViewSet(mixins.ListModelMixin,
+                  GenericViewSet):
+    queryset = Menu.objects.all()
+    serializer_class = MenuSerializer
+    filter_backends = (DjangoFilterBackend,)
+    filterset_fields = ('restaurant',)
