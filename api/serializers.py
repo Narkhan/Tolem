5ca1d3cdaf8f5f5f54c156eb39b70a0ce5428ba2
Narkhan Kamzabek <narkhan2002@gmail.com>
2022-07-28 00:02:45 +0600

    Implemented serializers for FoodItem, Category, Order, OrderItem models

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,13 @@
 from rest_framework import serializers
 
-from api.models.restaurant import Restaurant, Menu
+from api.models.restaurant import Restaurant, Menu, FoodItem, Category, Order, OrderItem
 
 
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
         fields = (
+            'id',
             'name',
             'address'
         )
@@ -16,6 +17,50 @@ class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
         fields = (
+            'id',
             'restaurant',
             'food_items'
+        )
+
+
+class FoodItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FoodItem
+        fields = (
+            'id',
+            'name',
+            'description',
+            'price',
+            'category',
+            'is_active'
+        )
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = (
+            'id',
+            'name'
+        )
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Order
+        fields = (
+            'id',
+            'restaurant',
+            'food_items'
+        )
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = (
+            'id',
+            'order',
+            'food_item',
+            'quantity'
         )
