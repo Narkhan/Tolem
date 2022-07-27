901467d8f43b5185be198e9e9f24625cde54bbd8
gmkanat <kanat_9999@inbox.ru>
2022-07-27 23:03:27 +0600

    refactored

--- api/serializers.py
+++ api/serializers.py
@@ -6,10 +6,16 @@ from api.models.restaurant import Restaurant, Menu
 class RestaurantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Restaurant
-        fields = ('name', 'address')
+        fields = (
+            'name',
+            'address'
+        )
 
 
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
-        fields = ('restaurant', 'food_items')
+        fields = (
+            'restaurant',
+            'food_items'
+        )
